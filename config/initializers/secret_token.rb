178e2c26875540610a6c441ef35456a08354cafc
Trevor Plant <tplant@dius.com.au>
2011-09-10 15:22:44 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MadeToMeasure::Application.config.secret_token = 'ed657da3f9c15094e5f96e78a907c2c793bfe02858d00fdaaa4954b683a4e3cdd721719e7d1f041cf4cde4aa114cd9fb78e2a1600042f023fd0d41d9f7412744'
